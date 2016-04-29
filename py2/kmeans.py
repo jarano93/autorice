bf24504df19415875567cd9e6fb600bd4a59eaa1
jarano93 <jarano93@gmail.com>
2016-04-29 00:37:48 -0700

    tested

--- py2/kmeans.py
+++ py2/kmeans.py
@@ -41,14 +41,15 @@ def getcolors(fName, portrait=True):
     # print vals
     return vals
 
-def kmeans(cvals, threshold=90, N=40, TOL=1e-1):
-    print 'running'
+def kmeans(cvals, top=4, N=50, threshold=90, TOL=1):
+    # print 'running'
     group16 = []
     rgb_old = []
     for i in xrange(len(seeds)):
         group16.append(Group(0, np.array(seeds[i]), []))
         rgb_old.append(np.array(seeds[i]))
     # calulate average while assigning initial classes -- use as BG
+    len_g = len(group16)
     sum = np.zeros(3)
     num_rgb = 0
     for col in cvals:
@@ -81,18 +82,17 @@ def kmeans(cvals, threshold=90, N=40, TOL=1e-1):
     for n in xrange(N):
         rgb_new = []
         # calculate new averages
-        for gr in group16:
-            if gr.n == 0:
-                rgb_new.append(gr.rgb)
+        for i in xrange(len_g):
+            if group16[i].n == 0:
+                rgb_new.append(group16[i].rgb)
             else:
                 new = np.zeros(3)
-                for key in gr.nrgbs:
+                for key in group16[i].nrgbs:
                     new += key[0] * key[1]
-                new = np.around( new / gr.n)
-                gr.rgb = new
+                new = np.around( new / group16[i].n)
+                group16[i].rgb = new
                 rgb_new.append(new)
-                gr.nrgbs = []
-                gr.n = 0
+                group16[i].nrgbs = []
 
         # if no change break
         max_diff = 0
@@ -100,19 +100,18 @@ def kmeans(cvals, threshold=90, N=40, TOL=1e-1):
             norm = la.norm(rgb_old[i] - rgb_new[i])
             if norm > max_diff:
                 max_diff = norm
-        print "%d: %f" % (n, max_diff)
+        # print "%d: %f" % (n, max_diff)
         if max_diff < TOL:
             break
 
-        # if np.array_equal(np.array(rgb_old), np.array(rgb_new)):
-            # print 'break %d' % (n)
-            # break
+        for i in xrange(len_g):
+            group16[i].n = 0
 
         # reassign groups using new averages
         for col in cvals:
             min = la.norm(255 * np.ones(3))
             key = 0
-            for j in xrange(len(group16)):
+            for j in xrange(len_g):
                 norm = la.norm(col[1] - group16[j].rgb)
                 if norm < min:
                     min = norm
@@ -123,12 +122,12 @@ def kmeans(cvals, threshold=90, N=40, TOL=1e-1):
         rgb_old = rgb_new
 
     # if a group has no points assign color to other nearest group
-    for i in xrange(len(group16)):
+    for i in xrange(len_g):
         if group16[i].n == 0:
             min = la.norm(group16[i].rgb - fg)
             color = fg
-            for j in xrange(len(group16)):
-                if i == j:
+            for j in xrange(len_g):
+                if group16[j].n == 0 or i == j:
                     continue
                 norm = la.norm(group16[i].rgb - group16[j].rgb)
                 if norm < min:
@@ -138,9 +137,20 @@ def kmeans(cvals, threshold=90, N=40, TOL=1e-1):
 
     color_vals = [rgb_to_hex(tuple(bg)), rgb_to_hex(tuple(fg))]
     hex_vals = []
-    for i in xrange(len(group16)):
+    for i in xrange(len_g):
         color_vals.append(rgb_to_hex(tuple(group16[i].rgb)))
-    # print color_vals
+    for t in xrange(top):
+        max = 0
+        key = 0
+        for i in xrange(len(group16)):
+            if group16[i].n > max:
+                max = group16[i].n
+                key = i
+        max_rgb = group16.pop(key).rgb
+        opp_rgb = 256 * np.ones(3) - max_rgb
+        color_vals.append(rgb_to_hex(tuple(max_rgb)))
+        color_vals.append(rgb_to_hex(tuple(opp_rgb)))
+
     return color_vals
 
 
@@ -160,8 +170,13 @@ def get_hex(rgb_list):
 def print_hex(hex_list):
     print "background %s" % (hex_list[0])
     print "foreground %s" % (hex_list[1])
-    for i in xrange(2, len(hex_list)):
-        print "color%d %s" %(i - 2, hex_list[i])
+    for i in xrange(2, 18):
+        print "color%d %s" % (i - 2, hex_list[i])
+    for i in xrange(18, len(hex_list)):
+        if i % 2 == 0:
+            print "top%d %s" % ((i - 18) / 2, hex_list[i])
+        else:
+            print "opp%d %s" % ((i - 19) / 2, hex_list[i])
 
 
 def colorscheme(imgName, outputDir="~/.config/colorschemes/"):
